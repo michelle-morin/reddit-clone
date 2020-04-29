230b37517ecc7ed0e586fe9b0e4464e6b33c9795
Michelle Morin <morin2m@gmail.com>
2020-04-28 17:22:49 -0700

    update initialstate filler text

--- src/components/InitialState.js
+++ src/components/InitialState.js
@@ -9,14 +9,14 @@ const InitialState = {
     [id1]: {
       title: "Cats are cool",
       body:
-        "Astrodonius Xuanhanosaurus Giganotosaurus Tendaguria Pinacosaurus Kaatedocus Troodon Dracopelta Coeluroides Banji Pterospondylus Agujaceratops Basutodon Epichirostenotes Nanosaurus Tawa Piveteausaurus Telmatosaurus Austrocheirus Herrerasaurus Stygivenator Sonidosaurus Protiguanodon Tatankaceratops Hypacrosaurus Angolatitan Europasaurus Yandusaurus Cruxicheiros Turiasaurus",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       upvote: 3,
       downvote: 2,
       postDate: 1588105367958,
       id: id1
     },
     [id2]: {
-      title: "Cats be like, WHAT!?!",
+      title: "My dog is cuter than your cat",
       body:
         "Astrodonius Xuanhanosaurus Giganotosaurus Tendaguria Pinacosaurus Kaatedocus Troodon Dracopelta Coeluroides Banji Pterospondylus Agujaceratops Basutodon Epichirostenotes Nanosaurus Tawa Piveteausaurus Telmatosaurus Austrocheirus Herrerasaurus Stygivenator Sonidosaurus Protiguanodon Tatankaceratops Hypacrosaurus Angolatitan Europasaurus Yandusaurus Cruxicheiros Turiasaurus",
       upvote: 10,
@@ -25,9 +25,9 @@ const InitialState = {
       id: id2
     },
     [id3]: {
-      title: "Fat Cats are the greatest",
+      title: "Check out this bird video",
       body:
-        "Astrodonius Xuanhanosaurus Giganotosaurus Tendaguria Pinacosaurus Kaatedocus Troodon Dracopelta Coeluroides Banji Pterospondylus Agujaceratops Basutodon Epichirostenotes Nanosaurus Tawa Piveteausaurus Telmatosaurus Austrocheirus Herrerasaurus Stygivenator Sonidosaurus Protiguanodon Tatankaceratops Hypacrosaurus Angolatitan Europasaurus Yandusaurus Cruxicheiros Turiasaurus",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris sit amet massa vitae tortor condimentum. Facilisis gravida neque convallis a cras. Est ullamcorper eget nulla facilisi. Vitae nunc sed velit dignissim sodales. Netus et malesuada fames ac turpis egestas integer eget aliquet. Suscipit adipiscing bibendum est ultricies. Varius sit amet mattis vulputate. Sit amet consectetur adipiscing elit pellentesque habitant. Vehicula ipsum a arcu cursus. Sit amet mattis vulputate enim nulla. Nisi est sit amet facilisis magna etiam tempor orci eu. Morbi non arcu risus quis varius. Adipiscing at in tellus integer feugiat scelerisque varius morbi enim.",
       upvote: 1,
       downvote: 2,
       postDate: 1588105407252,
