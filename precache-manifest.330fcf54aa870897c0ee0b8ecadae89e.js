a3cebe4aad3074b9af6236169efe41a134f56f72
Michelle Morin <morin2m@gmail.com>
2020-04-28 17:05:12 -0700

    Updates

---
+++ precache-manifest.330fcf54aa870897c0ee0b8ecadae89e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54b19514d315887c4612744f4a41a3e",
+    "url": "/reddit-clone/index.html"
+  },
+  {
+    "revision": "2812704d5dfa9520d10c",
+    "url": "/reddit-clone/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d51aeaf90fb20456858e",
+    "url": "/reddit-clone/static/css/main.f74953e5.chunk.css"
+  },
+  {
+    "revision": "2812704d5dfa9520d10c",
+    "url": "/reddit-clone/static/js/2.0b52e4fb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reddit-clone/static/js/2.0b52e4fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51aeaf90fb20456858e",
+    "url": "/reddit-clone/static/js/main.ade80809.chunk.js"
+  },
+  {
+    "revision": "7c87edb22a9ecbdc7b31",
+    "url": "/reddit-clone/static/js/runtime-main.3c754217.js"
+  }
+]);
