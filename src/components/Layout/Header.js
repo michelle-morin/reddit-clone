655082c0a36b2fb98072f9af6d42bc9e21ba688c
Michelle Morin <morin2m@gmail.com>
2020-04-28 17:17:59 -0700

    fix nav links

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,17 +6,11 @@ const Header = (props) => {
   return (
     <div style={headerStyles}>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="/">Reddit-Clone</Navbar.Brand>
+        <Navbar.Brand href="#">Reddit-Clone</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link onClick={props.whenButtonClick}>{props.buttonText}</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
